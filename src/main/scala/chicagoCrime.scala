37ef9fdac96ae4c24fc02f95b97904e677670bee
amgunden <amgunden@gmail.com>
2018-04-24 21:14:38 -0600

    Now reads in code with Beat numbers and replace old numbers with new ones

--- src/main/scala/chicagoCrime.scala
+++ src/main/scala/chicagoCrime.scala
@@ -1,6 +1,7 @@
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.{Row, SparkSession}
 import org.apache.spark.sql.types._
-import org.apache.spark.sql.functions.{hour, dayofmonth, month}
+import org.apache.spark.sql.functions.{dayofmonth, hour, map, month, udf}
 
 object chicagoCrime {
 
@@ -9,6 +10,11 @@ object chicagoCrime {
     val spark = SparkSession.builder().appName("chicagoCrime")
       .master("spark://concord.cs.colostate.edu:46010")
       .getOrCreate()
+
+    val beatSchema = StructType(Array(
+      StructField("actualBeat", IntegerType, true),
+      StructField("newBeat", IntegerType, true)
+    ))
 
     val customSchema = StructType(Array(
       StructField("id1", IntegerType, true),
@@ -36,6 +42,13 @@ object chicagoCrime {
       StructField("Location", StringType, true)
     ))
 
+    val beatData = spark.read.format("csv")
+      .option("header", "false")
+      .schema(beatSchema)
+      .load(args(1))
+      .persist()
+
+
     // Read in CSV file
     val crime = spark.read.format("csv")
       .option("header", "true")
@@ -45,13 +58,28 @@ object chicagoCrime {
       .persist()
 
 
+    val beatRdd = beatData.rdd.map{
+      case Row(actualBeat: Int, newBeat: Int) => (actualBeat, newBeat)
+    }.collectAsMap()
+
+
+    def func: (Int => Int) = {b => beatRdd.get(b).get}
+
+    val correctBeats = udf(func)
+
+//    val withCorrBeat = crime.withColumn("adjBeats", correctBeats(crime("Beat")))
+
+//    println(withCorrBeat.show(25))
+
+
     // Test extraction of hour
     val additions = crime
       .withColumn("hourOfDay", hour(crime("Date")))
       .withColumn("day", dayofmonth(crime("Date")))
       .withColumn("month", month(crime("Date")))
+      .withColumn("adjBeats", correctBeats(crime("Beat")))
 
-    val set = additions.select("year", "month", "day", "hourOfDay", "IUCR", "Beat")
+    val set = additions.select("year", "month", "day", "hourOfDay", "IUCR", "Beat", "adjBeats")
 
     println(set.show(25))
 
